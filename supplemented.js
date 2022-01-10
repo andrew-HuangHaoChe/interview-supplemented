4168212e4d097f4cf6d0894a56c52af836ef9f3e
Huang Hao Che <62294412+andrew-HuangHaoChe@users.noreply.github.com>
2022-01-11 00:29:02 +0800

    Update supplemented.js

--- supplemented.js
+++ supplemented.js
@@ -13,12 +13,51 @@ document.write(temp);
 
 
 // bmi
-function calcBmi (weight, height) {
-    let w = parseInt(weight);
-    let h = parseInt(height)/100;
-    let bmi = ((w/(h*h))).toFixed(2);
-    return bmi
+function judgebmi(gender, bmi) {
+    if (gender == 'boy') {
+        if (bmi < 18.5) {
+            console.log('體重過輕');
+        } else if (bmi >= 18.5 && bmi < 24) {
+            console.log('體重正常');
+        } else if (bmi >= 24 && bmi < 27) {
+            console.log('過重');
+        } else if (bmi >= 27 && bmi < 30) {
+            console.log('輕度肥胖');
+        } else if (bmi >= 30 && bmi < 35) {
+            console.log('中度肥胖');
+        } else if (bmi >= 35) {
+            console.log('重度肥胖');
+        }
+
+        if (bmi >= 24) {
+            console.log('腰圍 >= 90cm !');
+        }
+    } else if (gender == 'girl') {
+        if (bmi < 18.5) {
+            console.log('體重過輕');
+        } else if (bmi >= 18.5 && bmi < 24) {
+            console.log('體重正常');
+        } else if (bmi >= 24 && bmi < 27) {
+            console.log('過重');
+        } else if (bmi >= 27 && bmi < 30) {
+            console.log('輕度肥胖');
+        } else if (bmi >= 30 && bmi < 35) {
+            console.log('中度肥胖');
+        } else if (bmi >= 35) {
+            console.log('重度肥胖');
+        }
+
+        if (bmi >= 24) {
+            console.log('腰圍 >= 80cm !');
+        }
+    }
 }
 
-const bmi = calcBmi(80, 180);
-console.log(bmi);
+function calcbmi(gender, weight, height) {
+    let w = parseInt(weight);
+    let h = parseInt(height) / 100;
+    let bmi = ((w / (h * h))).toFixed(2);
+    judgebmi(gender, bmi);
+}
+
+calcbmi('boy', 80, 182);
